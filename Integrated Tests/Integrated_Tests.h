7f93fbca8e80488787a9c241f7519cb467afba96
Ryan Davies <ryan@ryandavies.net>
2013-04-05 13:48:41 +0100

    Added integrated tests target.

---
+++ Integrated Tests/Integrated_Tests.h
@@ -0,0 +1,13 @@
+//
+//  Integrated_Tests.h
+//  Integrated Tests
+//
+//  Created by Ryan Davies on 05/04/2013.
+//  Copyright (c) 2013 Ryan Davies. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Integrated_Tests : SenTestCase
+
+@end
---
+++ Integrated Tests/Integrated_Tests.m
@@ -0,0 +1,32 @@
+//
+//  Integrated_Tests.m
+//  Integrated Tests
+//
+//  Created by Ryan Davies on 05/04/2013.
+//  Copyright (c) 2013 Ryan Davies. All rights reserved.
+//
+
+#import "Integrated_Tests.h"
+
+@implementation Integrated_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Integrated Tests");
+}
+
+@end
