9e3f6f52a91f147e782e0698489e682dc79ca14d
Ryan Davies <ryan@ryandavies.net>
2013-04-05 13:51:31 +0100

    Commit directory rename from CSVTests -> Unit Tests. Xcode failed to notice.

---
+++ Unit Tests/Tests.h
@@ -0,0 +1,13 @@
+//
+//  CSVTests.h
+//  CSVTests
+//
+//  Created by Ryan Davies on 05/04/2013.
+//  Copyright (c) 2013 Ryan Davies. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CSVTests : SenTestCase
+
+@end
---
+++ Unit Tests/Tests.m
@@ -0,0 +1,15 @@
+//
+//  CSVTests.m
+//  CSVTests
+//
+//  Created by Ryan Davies on 05/04/2013.
+//  Copyright (c) 2013 Ryan Davies. All rights reserved.
+//
+
+#import "CSVTests.h"
+
+@implementation CSVTests
+
+
+
+@end
